bad4b00257b4d526f4ac0a5ef064bc2f7c33e966
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-03-05 15:55:20 +0100

    :sparkles: Src > Index.js > Savoir project image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 //////////////////////// IMPORTS ////////////////////////
 // Stylus
 import './style/main.styl'
+
+//////////////////////// IMAGES ////////////////////////
+// Savoir
+import savoirImage from './images/savoir.png'
+
+const savoirImageContainer = document.querySelector('.js-savoir-image')
+const $savoirImage = new Image()
+$savoirImage.src = savoirImage
+savoirImageContainer.appendChild($savoirImage)
