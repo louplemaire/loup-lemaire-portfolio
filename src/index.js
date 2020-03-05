aa5fb8e0bee0a3fbf02f3a2ea5499ac8075c6319
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-03-05 20:03:26 +0100

    :beers: :sparkles: Src > Index.js > Add ode brew grinder images

--- src/index.js
+++ src/index.js
@@ -10,3 +10,11 @@ const savoirImageContainer = document.querySelector('.js-savoir-image')
 const $savoirImage = new Image()
 $savoirImage.src = savoirImage
 savoirImageContainer.appendChild($savoirImage)
+
+// Ode Brew Grinder
+import odeBrewGrinderImage from './images/ode-brew-grinder.png'
+
+const odeBrewGrinderImageContainer = document.querySelector('.js-ode-brew-grinder-image')
+const $odeBrewGrinderImage = new Image()
+$odeBrewGrinderImage.src = odeBrewGrinderImage
+odeBrewGrinderImageContainer.appendChild($odeBrewGrinderImage)
