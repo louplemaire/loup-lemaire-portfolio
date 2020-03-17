87d4d7199d2a45832df7c8c698e12f4bceff796e
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-03-17 11:27:29 +0100

    :sparkles: Src > Index.js > Add Loup image and add alt for all images

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const savoirImageContainer = document.querySelector('.js-savoir-image')
 const $savoirImage = new Image()
 $savoirImage.src = savoirImage
 savoirImageContainer.appendChild($savoirImage)
+$savoirImage.setAttribute('alt', 'SAVOIR image')
 
 // Ode Brew Grinder
 import odeBrewGrinderImage from './images/ode-brew-grinder.png'
@@ -22,6 +23,7 @@ const odeBrewGrinderImageContainer = document.querySelector('.js-ode-brew-grinde
 const $odeBrewGrinderImage = new Image()
 $odeBrewGrinderImage.src = odeBrewGrinderImage
 odeBrewGrinderImageContainer.appendChild($odeBrewGrinderImage)
+$odeBrewGrinderImage.setAttribute('alt', 'Ode Brew Grinder image')
 
 // EO Securite
 import eoSecuriteImage from './images/eo-securite.png'
@@ -30,11 +32,22 @@ const eoSecuriteImageContainer = document.querySelector('.js-eo-securite-image')
 const $eoSecuriteImage = new Image()
 $eoSecuriteImage.src = eoSecuriteImage
 eoSecuriteImageContainer.appendChild($eoSecuriteImage)
+$eoSecuriteImage.setAttribute('alt', 'EO Sécurité image')
 
 // Boveroux
 import boverouxImage from './images/boveroux.png'
 
-const boverouxContainer = document.querySelector('.js-boveroux-image')
-const $boveroux = new Image()
-$boveroux.src = boverouxImage
-boverouxContainer.appendChild($boveroux)
+const boverouxImageContainer = document.querySelector('.js-boveroux-image')
+const $boverouxImage = new Image()
+$boverouxImage.src = boverouxImage
+boverouxImageContainer.appendChild($boverouxImage)
+$boverouxImage.setAttribute('alt', 'Boveroux & Fils image')
+
+// Loup
+import loupImage from './images/loup-lemaire.jpg'
+
+const loupImageContainer = document.querySelector('.js-loup-image')
+const $loupImage = new Image()
+$loupImage.src = loupImage
+loupImageContainer.appendChild($loupImage)
+$loupImage.setAttribute('alt', 'Loup Lemaire image')
