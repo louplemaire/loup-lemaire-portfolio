74c5dc9f17e910726e8f9c5686f2a71e21740d3f
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-05-25 10:53:27 +0200

    :sparkles: > Src > Index.js > Add three.js home

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@
 // Stylus
 import './style/main.styl'
 
+// Three.js home
+import * as THREE from 'three'
+import particleImageSource from './assets/particle/bubble.png'
+
 /**
  * Images
  */
 // Savoir
-import savoirImage from './images/savoir.png'
+import savoirImage from './assets/images/savoir.png'
 
 const savoirImageContainer = document.querySelector('.js-savoir-image')
 const $savoirImage = new Image()
@@ -17,7 +21,7 @@ savoirImageContainer.appendChild($savoirImage)
 $savoirImage.setAttribute('alt', 'SAVOIR image')
 
 // Ode Brew Grinder
-import odeBrewGrinderImage from './images/ode-brew-grinder.png'
+import odeBrewGrinderImage from './assets/images/ode-brew-grinder.png'
 
 const odeBrewGrinderImageContainer = document.querySelector('.js-ode-brew-grinder-image')
 const $odeBrewGrinderImage = new Image()
@@ -26,7 +30,7 @@ odeBrewGrinderImageContainer.appendChild($odeBrewGrinderImage)
 $odeBrewGrinderImage.setAttribute('alt', 'Ode Brew Grinder image')
 
 // EO Securite
-import eoSecuriteImage from './images/eo-securite.png'
+import eoSecuriteImage from './assets/images/eo-securite.png'
 
 const eoSecuriteImageContainer = document.querySelector('.js-eo-securite-image')
 const $eoSecuriteImage = new Image()
@@ -35,7 +39,7 @@ eoSecuriteImageContainer.appendChild($eoSecuriteImage)
 $eoSecuriteImage.setAttribute('alt', 'EO Sécurité image')
 
 // Boveroux
-import boverouxImage from './images/boveroux.png'
+import boverouxImage from './assets/images/boveroux.png'
 
 const boverouxImageContainer = document.querySelector('.js-boveroux-image')
 const $boverouxImage = new Image()
@@ -44,7 +48,7 @@ boverouxImageContainer.appendChild($boverouxImage)
 $boverouxImage.setAttribute('alt', 'Boveroux & Fils image')
 
 // Loup
-import loupImage from './images/loup-lemaire.jpg'
+import loupImage from './assets/images/loup-lemaire.jpg'
 
 const loupImageContainer = document.querySelector('.js-loup-image')
 const $loupImage = new Image()
@@ -281,13 +285,22 @@ logos.forEach(_logo => {
 })
 
 /**
+ * Sizes for canvas cursor and three.js home
+ */
+const sizes = {}
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
+
+/**
  * Canvas cursor
  */
-const $canvas = document.querySelector('canvas')
+const $canvas = document.querySelector('.js-canvas-cursor')
 const context = $canvas.getContext('2d')
 
 // Cursor
-const cursor = { x: 0, y: 0 }
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
 
 window.addEventListener('mousemove', (_event) => {
     cursor.x = _event.clientX
@@ -295,7 +308,10 @@ window.addEventListener('mousemove', (_event) => {
 })
 
 // Ball
-const ball = { x: 200, y: 200, radius: 18 }
+const ball = {}
+ball.x = 200
+ball.y = 200
+ball.radius = 18
 
 // Hover animation
 window.addEventListener('mouseover', (_event) => {
@@ -306,10 +322,89 @@ window.addEventListener('mouseover', (_event) => {
     }
 })
 
-// Loop
+// Resize
+const canvasCursorResize = () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    $canvas.width = sizes.width
+    $canvas.height = sizes.height
+}
+
+window.addEventListener('resize', canvasCursorResize)
+canvasCursorResize()
+
+/**
+ * Three.js home
+ */
+// Scene
+const scene = new THREE.Scene()
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 8
+scene.add(camera)
+
+// Geometry
+const particlesGeometry = new THREE.Geometry()
+
+for(let i = 0; i < 500; i++){
+    // Vertice
+    const vertice = new THREE.Vector3(
+        (Math.random() - 0.5) * 10,
+        (Math.random() - 0.5) * 10,
+        (Math.random() - 0.5) * 10
+    )
+    particlesGeometry.vertices.push(vertice)
+}
+
+// Material
+const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load(particleImageSource)
+
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.2,
+    sizeAttenuation: true,
+    alphaMap: particleTexture,
+    transparent: true,
+    color: new THREE.Color(0XFAF8F8),
+    blending: THREE.AdditiveBlending,
+    depthWrite: false
+})
+
+// Particles
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+// Renderer
+const home = document.querySelector('.js-home')
+const renderer = new THREE.WebGLRenderer({ alpha: true })
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setClearAlpha(0)
+renderer.domElement.classList.add('three-js-home')
+home.appendChild(renderer.domElement)
+
+// Resize
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+/**
+ * Loop
+ */
 const loop = () => {
     window.requestAnimationFrame(loop)
 
+    /**
+     * Canvas cursor
+     */
     // Update position
     ball.x += (cursor.x - ball.x) * 0.1
     ball.y += (cursor.y - ball.y) * 0.1
@@ -323,21 +418,19 @@ const loop = () => {
     context.globalAlpha = 1
     context.strokeStyle = '#E40404'
     context.stroke()
+
+    /**
+     * Three.js home
+     */
+    // Particles
+    particles.rotation.y += 0.003
+    particles.rotation.z += 0.002
+
+    // Camera
+    camera.lookAt(scene.position)
+
+    // Render
+    renderer.render(scene, camera)
 }
 
 loop()
-
-// Sizes
-const sizes = { width: null, height: null }
-
-// Resize
-const resize = () => {
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    $canvas.width = sizes.width
-    $canvas.height = sizes.height
-}
-
-window.addEventListener('resize', resize)
-resize()
